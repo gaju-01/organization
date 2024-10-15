92b6d76d4e358d27d6d67fbc1745610d0761f86c
gaju-01 <you@example.com>
2024-10-15 23:17:55 +0530

    Added localResolver and messageSource configs

--- src/main/java/com/microservices/organization/OrganizationApplication.java
+++ src/main/java/com/microservices/organization/OrganizationApplication.java
@@ -3,6 +3,12 @@ package com.microservices.organization;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
 @RefreshScope
@@ -10,6 +16,21 @@ public class OrganizationApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OrganizationApplication.class, args);
+	}
+
+	@Bean
+	public LocaleResolver localeResolver() {
+		SessionLocaleResolver sessionLocaleResolver = new SessionLocaleResolver();
+		sessionLocaleResolver.setDefaultLocale(Locale.US);
+		return sessionLocaleResolver;
+	}
+
+	@Bean
+	public ResourceBundleMessageSource messageSource() {
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setUseCodeAsDefaultMessage(true);
+		messageSource.setBasename("messages");
+		return messageSource;
 	}
 
 }
